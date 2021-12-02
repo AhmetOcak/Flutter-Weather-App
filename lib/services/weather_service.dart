c544a85933e7b6ddb4e811c9cd8a4b5d1db4934f
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-02 20:46:20 +0300

    api connection

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,24 @@
+
+
+import 'dart:convert';
+
+import 'package:wheather_app/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class DataService {
+  
+  Future<WeatherResponse> getWeatherData(String city) async {
+    // https://api.openweathermap.org/data/2.5/weather?q=isparta&units=metric&appid=18fcba115a2815e21a21379f65b994ed
+
+    final queryParameters = {
+      'appid': '18fcba115a2815e21a21379f65b994ed',
+      'q': city,
+      'units': 'metric'
+    };
+
+    final uri = Uri.https('api.openweathermap.org', 'data/2.5/weather', queryParameters);
+    final response = await http.get(uri);
+    final json = jsonDecode(response.body);
+    return WeatherResponse.fromJson(json);
+  }
+}

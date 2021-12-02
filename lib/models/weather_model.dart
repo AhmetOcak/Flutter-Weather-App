c547938a989b751f190783b1d38c1ab9808005c5
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-02 20:46:33 +0300

    model added

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,38 @@
+
+class WeatherResponse {
+  final String? cityName;
+  final String? temp;
+  final String? feelsLike;
+  final String? maxTemp;
+  final String? minTemp;
+  final String? pressure;
+  final String? windSpeed;
+  final String? sunRise;
+  final String? sunSet;
+  final String? humidity;
+  final String? description;
+  final String? icon;
+
+  WeatherResponse({
+    this.cityName, this.temp, this.feelsLike, this.maxTemp, this.minTemp, 
+    this.pressure, this.windSpeed, this.sunRise, this.sunSet, this.humidity, this.description, this.icon});
+
+  factory WeatherResponse.fromJson(Map<String, dynamic> json) {
+    final cityName = json['name'].toString();
+    final temp = json['main']['temp'].toString();
+    final feelsLike = json['main']['feels_like'].toString();
+    final maxTemp = json['main']['temp_max'].toString();
+    final minTemp = json['main']['temp_min'].toString();
+    final pressure = json['main']['pressure'].toString();
+    final windSpeed = json['wind']['speed'].toString();
+    final sunRise = json['sys']['sunrise'].toString();
+    final sunSet = json['sys']['sunset'].toString();
+    final humidity = json['main']['humidity'].toString();
+    final description = json['weather'][0]['description'].toString();
+    final icon = json['weather'][0]['icon'];
+
+    return WeatherResponse(
+      cityName: cityName, temp: temp, feelsLike: feelsLike, maxTemp: maxTemp, minTemp: minTemp, pressure: 
+      pressure, windSpeed: windSpeed, sunRise: sunRise, sunSet: sunSet, humidity: humidity, description: description, icon: icon);
+  }
+}

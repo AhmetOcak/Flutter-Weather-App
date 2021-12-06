3bace821be22b97e34e85c8edbfbf76399816909
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-06 14:20:53 +0300

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wheather_app/views/home_screen.dart';
 import 'package:wheather_app/views/search_city_screen.dart';
 
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -69,4 +69,5 @@ class _HomeScreenState extends State<HomeScreen> {
       _weatherResponse = response;
     });
   }
+  
 }
